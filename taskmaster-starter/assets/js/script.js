198fdfa8699b5de89ad033c892fe6014deb5228e
borielvi <borielvi@isu.edu>
2020-06-23 17:10:59 -0700

    added drag and drop

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -92,6 +92,74 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+
+  update: function(event) {
+    // loop over current set of children in sortable list
+    // array to store the task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    })
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
@@ -119,15 +187,24 @@ $(".list-group").on("click", "p", function() {
   });
 
   // recreate p element
-var taskP = $("<p>")
-.addClass("m-1")
-.text(text);
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
 
-// replace textarea with p element
-$(this).replaceWith(taskP);
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
   var text = $(this)
     .text()
     .trim();
+
+  var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus");
+
 });
 
 
@@ -148,14 +225,6 @@ $("#task-form-modal .btn-primary").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
 
-  var textInput = $("<textarea>")
-  .addClass("form-control")
-  .val(text);
-
-  $(this).replaceWith(textInput);
-
-  textInput.trigger("focus");
-
   var taskDate = $("#modalDueDate").val();
 
   if (taskText && taskDate) {
@@ -169,7 +238,7 @@ $("#task-form-modal .btn-primary").click(function() {
       text: taskText,
       date: taskDate
     });
-
+    
     saveTasks();
   }
 });
